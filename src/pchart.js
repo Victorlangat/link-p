2eaee52b73fd085b778e5b8d31ff6130bd0b6bc7
Victorlangat <111707177+Victorlangat@users.noreply.github.com>
2024-04-26 08:06:08 +0300

    2nd commit

--- src/pchart.js
+++ src/pchart.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import {Pie} from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 import {Chart, ArcElement} from 'chart.js';
 
 Chart.register(ArcElement);
 
-function PieChart(){
+function PieChart() {
     const data = {
-        labels: ['Paypal', 'M-pesa', 'Crypto', 'Credit-cards', 'Debit-cards'],
+        labels: ['m-pesa', 'paypal', 'credit-cards', 'crypto', 'edge-cards',],
         datasets: [
             {
-                data: [300, 50, 100, 200, 300],
-                backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)','rgba(30,25,122,0.5)', 'rgba(0,0,0,0.5)'],
-                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+                data: [12, 19, 3, 5, 2],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
                 borderWidth: 1,
             },
         ],
@@ -21,3 +34,4 @@ function PieChart(){
 }
 
 export default PieChart;
+                 
